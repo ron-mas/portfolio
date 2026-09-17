25bb499ab9f89a7006b8d03f660ff5e2aa111d3f
ron-mas <n.massuu@gmail.com>
2026-09-18 02:28:22 +0900

    Add files via upload

--- data.js
+++ data.js
@@ -1,53 +1,38 @@
 const WORKS_DATA = [
+    
+    // 01_bokupoke
+
   {
-    "slug": "dream-live-2016",
-    "title": "ミュージカル『テニスの王子様』3rdシーズン Dream Live 2016",
-    "shortTitle": "Dream Live 2016",
-    "year": 2016,
+    "slug": "01_bokupoke",
+    "title": "「ぼくらの甲子園！ポケット」イラスト",
+    "year": 2013,
     "yearLabel": "2013 - 2014",
     "category": [
-      "web",
-      "advertising",
-      "goods",
       "illustration"
     ],
-    "thumbnail": "assets/sample-dream-live.svg",
-    "overview": "ミュージカル『テニスの王子様』3rdシーズン Dream Live 2016のWebプロモーション施策です。「テニミュガチャ」と題し、毎日キャストのカード画像が当たるガチャを楽しめる企画を立案し、公演への期待感を高めるWebコンテンツとして展開しました。",
+    "thumbnail": "assets/01_bokupoke/01_1.png",
+    "overview": "",
     "company": "面白法人カヤック",
     "tools": "Adobe Illustrator",
-    "areas": "キャラクターアバター / 服飾アイテム",
-    "roles": "イラストレーター・グラフィックデザイナー・Webデザイナー・グッズデザイナー・ディレクター・企画"
+    "areas": "キャラクターアバター / アイテム",
+    "roles": "イラスト"
   },
+
+    // 02_pokekoro
+
   {
-    "slug": "dream-life-2017",
-    "title": "ミュージカル『テニスの王子様』3rdシーズン Dream Live 2017「Dream Life」",
-    "shortTitle": "Dream Life 2017",
-    "year": 2017,
-    "yearLabel": "2017",
+    "slug": "02_pokekoro",
+    "title": "「ポケコロ」アバターアイテムイラスト",
+    "year": 2015,
+    "yearLabel": "2015",
     "category": [
-      "web"
+      "illustration"
     ],
-    "thumbnail": "assets/sample-dream-life.svg",
-    "overview": "前年に引き続き、Dream LiveのWebプロモーション施策として、日替わりで電話風のボイスコンテンツを楽しめる「Dream Life」を企画・制作しました。",
-    "company": "面白法人カヤック",
-    "tools": "Adobe Illustrator / Photoshop",
-    "areas": "Webデザイン / 素材制作 / ボイス編集",
-    "roles": "ディレクター・Webデザイナー"
-  },
-  {
-    "slug": "dream-life-2-2018",
-    "title": "ミュージカル『テニスの王子様』15周年記念コンサート Dream Live 2018「Dream Life 2」",
-    "shortTitle": "Dream Life 2",
-    "year": 2018,
-    "yearLabel": "2018",
-    "category": [
-      "web"
-    ],
-    "thumbnail": "assets/sample-dream-life2.svg",
-    "overview": "前年に好評だった「Dream Life」をバージョンアップ。「Dream Life 2」と題し、日替わりでビデオ通話風の動画を楽しめるプロモーションサイトを企画・制作しました。",
-    "company": "面白法人カヤック",
-    "tools": "Adobe Illustrator / Photoshop",
-    "areas": "Webデザイン / 素材制作 / 動画編集",
-    "roles": "ディレクター・Webデザイナー"
+    "thumbnail": "assets/02_pokekoro/02_1.png",
+    "overview": "",
+    "company": "ココネ株式会社",
+    "tools": "Adobe Illustrator / Adobe Flash",
+    "areas": "",
+    "roles": "イラスト"
   }
-];
+]
