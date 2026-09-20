e0eee43898e9d6a0d36ad827302cb50c22489cdf
ron-mas <n.massuu@gmail.com>
2026-09-21 02:24:46 +0900

    Update data.js

--- data.js
+++ data.js
@@ -48,4 +48,49 @@ const WORKS_DATA = [
     "tools": "Photoshop / Illustrator / Flash",
     "areas": "webデザイン / ガチャ演出 / 画像制作 / 企画 / ディレクション",
   },
+
+    {
+    "slug": "04_tenimyu2",
+    "title": "Dream Live 2017 特設サイト",
+    "year": 2017,
+    "yearLabel": "2017",
+    "category": [
+      "web"
+    ],
+    "thumbnail": "assets/04_tenimyu2/04_1.png",
+    "overview": "ミュージカル『テニスの王子様』3rdシーズン Dream Live 2017のWebプロモーション施策です。\n\n前年に引き続き、Dream Liveというタイトルにかけ、「Dream Life」をテーマに、日替わりで電話風のボイスコンテンツを楽しめるプロモーションサイトを企画・制作しました。\n\n企画立案から、全体の進行管理、Webデザイン、日替わりTOP画像やアイコンなどの素材制作、ボイスの編集まで、一貫して担当しました。",
+    "company": "re-vue合同会社",
+    "tools": "Photoshop / Illustrator / Wave Pad",
+    "areas": "webデザイン / 画像制作 / 音声編集 / 企画 / ディレクション",
+  },
+
+    {
+    "slug": "05_tenimyu3",
+    "title": "Dream Live 2018 特設サイト",
+    "year": 2018,
+    "yearLabel": "2018",
+    "category": [
+      "web"
+    ],
+    "thumbnail": "assets/05_tenimyu3/05_1.png",
+    "overview": "ミュージカル『テニスの王子様』15周年記念コンサート Dream Live 2018のWebプロモーション施策です。\n\n毎年恒例となっているDream LiveのWebプロモーションとして、前年に好評だった「Dream Life」をバージョンアップ。「Dream Life 2」と題し、日替わりでビデオ通話風の動画を楽しめるプロモーションサイトを企画・制作しました。\n\n企画立案から、全体の進行管理、Webデザイン、アイコンやバナーなどの素材制作、動画の編集まで、一貫して担当しました。",
+    "company": "re-vue合同会社",
+    "tools": "Photoshop / Illustrator / Premiere Pro",
+    "areas": "webデザイン / 画像制作 / 動画編集 / 企画 / ディレクション",
+  },
+
+    {
+    "slug": "06_ansute",
+    "title": "あんステTSF 特設サイト",
+    "year": 2017,
+    "yearLabel": "2017-2018",
+    "category": [
+      "web"
+    ],
+    "thumbnail": "assets/06_ansute/06_1.png",
+    "overview": "『あんさんぶるスターズ！オン・ステージ』～To the shining future～のWebプロモーション施策です。\n\n2016年に制作した「テニミュガチャ」と同様に、キャストの画像が排出されるガチャ施策を企画しました。作品の世界観に合わせて、デザインを一新しています。\n\n企画立案から、ディレクターとしての全体進行管理、Webデザイン、カードやバナーなどの素材制作、ガチャ演出まで担当しました。",
+    "company": "re-vue合同会社",
+    "tools": "Photoshop / Illustrator / Animate",
+    "areas": "webデザイン / 画像制作 / ガチャ演出 / 企画 / ディレクション",
+  },
 ]
