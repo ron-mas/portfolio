3f978c2a2f56bc198826cb38f2a54f4e932692f5
ron-mas <n.massuu@gmail.com>
2026-09-21 02:54:20 +0900

    Update data.js

--- data.js
+++ data.js
@@ -5,7 +5,6 @@ const WORKS_DATA = [
   {
     "slug": "01_bokupoke",
     "title": "「ぼくらの甲子園！ポケット」イラスト",
-    "year": 2013,
     "yearLabel": "2013/8 - 2014/12",
     "sortDate": "2013/8",
     "category": [
@@ -23,7 +22,6 @@ const WORKS_DATA = [
   {
     "slug": "02_pokekoro",
     "title": "「ポケコロ」アバターアイテムイラスト",
-    "year": 2015,
     "yearLabel": "2015/1 - 2015/8",
     "sortDate": "2015/1",
     "category": [
@@ -41,14 +39,13 @@ const WORKS_DATA = [
     {
     "slug": "03_tenimyu1",
     "title": "Dream Live 2016 特設サイト",
-    "year": 2016,
     "yearLabel": "2016/2 - 2016/4",
     "sortDate": "2016/2",
     "category": [
       "web"
     ],
     "thumbnail": "assets/03_tenimyu1/03_1.png",
-    "overview": "ミュージカル『テニスの王子様』3rdシーズン Dream Live 2016のWebプロモーション施策です。「テニミュガチャ」という毎日キャストのカード画像が当たるガチャを楽しめる企画を立案し、公演への期待感を高めるWebコンテンツとして展開しました。企画立案から、全体進行管理、Webデザイン、カードや日替わりアイコンなどの素材制作まで、一貫して担当しました。",
+    "overview": "ミュージカル『テニスの王子様』3rdシーズン Dream Live 2016のWebプロモーション施策です。\n「テニミュガチャ」という毎日キャストのカード画像が当たるガチャを楽しめる企画を立案し、公演への期待感を高めるWebコンテンツとして展開しました。\n企画立案から、全体進行管理、Webデザイン、カードや日替わりアイコンなどの素材制作まで、一貫して担当しました。",
     "company": "re-vue合同会社 / 株式会社マーベラス",
     "tools": "Photoshop / Illustrator / Flash",
     "areas": "webデザイン / ガチャ演出 / 画像制作 / 企画 / ディレクション",
@@ -59,14 +56,13 @@ const WORKS_DATA = [
     {
     "slug": "04_tenimyu2",
     "title": "Dream Live 2017 特設サイト",
-    "year": 2017,
     "yearLabel": "2017/3 - 2017/5",
     "sortDate": "2017/3",
     "category": [
       "web"
     ],
     "thumbnail": "assets/04_tenimyu2/04_1.png",
-    "overview": "ミュージカル『テニスの王子様』3rdシーズン Dream Live 2017のWebプロモーション施策です。\n\n前年に引き続き、Dream Liveというタイトルにかけ、「Dream Life」をテーマに、日替わりで電話風のボイスコンテンツを楽しめるプロモーションサイトを企画・制作しました。\n\n企画立案から、全体の進行管理、Webデザイン、日替わりTOP画像やアイコンなどの素材制作、ボイスの編集まで、一貫して担当しました。",
+    "overview": "ミュージカル『テニスの王子様』3rdシーズン Dream Live 2017のWebプロモーション施策です。\n前年に引き続き、Dream Liveというタイトルにかけ、「Dream Life」をテーマに、日替わりで電話風のボイスコンテンツを楽しめるプロモーションサイトを企画・制作しました。\n企画立案から、全体の進行管理、Webデザイン、日替わりTOP画像やアイコンなどの素材制作、ボイスの編集まで、一貫して担当しました。",
     "company": "re-vue合同会社 / 株式会社マーベラス",
     "tools": "Photoshop / Illustrator / Wave Pad",
     "areas": "webデザイン / 画像制作 / 音声編集 / 企画 / ディレクション",
@@ -77,14 +73,13 @@ const WORKS_DATA = [
     {
     "slug": "05_tenimyu3",
     "title": "Dream Live 2018 特設サイト",
-    "year": 2018,
     "yearLabel": "2018/3 - 2018/5",
     "sortDate": "2018/3",
     "category": [
       "web"
     ],
     "thumbnail": "assets/05_tenimyu3/05_1.png",
-    "overview": "ミュージカル『テニスの王子様』15周年記念コンサート Dream Live 2018のWebプロモーション施策です。\n\n毎年恒例となっているDream LiveのWebプロモーションとして、前年に好評だった「Dream Life」をバージョンアップ。「Dream Life 2」と題し、日替わりでビデオ通話風の動画を楽しめるプロモーションサイトを企画・制作しました。\n\n企画立案から、全体の進行管理、Webデザイン、アイコンやバナーなどの素材制作、動画の編集まで、一貫して担当しました。",
+    "overview": "ミュージカル『テニスの王子様』15周年記念コンサート Dream Live 2018のWebプロモーション施策です。\n毎年恒例となっているDream LiveのWebプロモーションとして、前年に好評だった「Dream Life」をバージョンアップ。「Dream Life 2」と題し、日替わりでビデオ通話風の動画を楽しめるプロモーションサイトを企画・制作しました。\n企画立案から、全体の進行管理、Webデザイン、アイコンやバナーなどの素材制作、動画の編集まで、一貫して担当しました。",
     "company": "re-vue合同会社 / 株式会社マーベラス",
     "tools": "Photoshop / Illustrator / Premiere Pro",
     "areas": "webデザイン / 画像制作 / 動画編集 / 企画 / ディレクション",
@@ -95,14 +90,13 @@ const WORKS_DATA = [
     {
     "slug": "06_ansute",
     "title": "あんステTSF 特設サイト",
-    "year": 2017,
     "yearLabel": "2017/11 - 2018/1",
     "sortDate": "2017-11",
     "category": [
       "web"
     ],
     "thumbnail": "assets/06_ansute/06_1.png",
-    "overview": "『あんさんぶるスターズ！オン・ステージ』～To the shining future～のWebプロモーション施策です。\n\n2016年に制作した「テニミュガチャ」と同様に、キャストの画像が排出されるガチャ施策を企画しました。作品の世界観に合わせて、デザインを一新しています。\n\n企画立案から、ディレクターとしての全体進行管理、Webデザイン、カードやバナーなどの素材制作、ガチャ演出まで担当しました。",
+    "overview": "『あんさんぶるスターズ！オン・ステージ』～To the shining future～のWebプロモーション施策です。\n2016年に制作した「テニミュガチャ」と同様に、キャストの画像が排出されるガチャ施策を企画しました。作品の世界観に合わせて、デザインを一新しています。\n企画立案から、ディレクターとしての全体進行管理、Webデザイン、カードやバナーなどの素材制作、ガチャ演出まで担当しました。",
     "company": "re-vue合同会社 / 株式会社マーベラス",
     "tools": "Photoshop / Illustrator / Animate",
     "areas": "webデザイン / 画像制作 / ガチャ演出 / 企画 / ディレクション",
